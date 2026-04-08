8268736e57356f82c3606e667c761297d2377ef4
SLASHBUTCODING <22-01684@g.batstate-u.edu.ph>
2026-04-08 13:54:12 +0800

    Fix search modal blank - remove overflow-hidden from root div that was clipping the absolute modal

--- src/app/pages/customer/Home.tsx
+++ src/app/pages/customer/Home.tsx
@@ -236,7 +236,7 @@ export function CustomerHome() {
   };
 
   return (
-    <div className="relative flex flex-col h-full overflow-hidden">
+    <div className="relative flex flex-col h-full">
       {/* Map Container - Fixed 42% height */}
       <div className="h-[42vh] flex-shrink-0 relative overflow-hidden">
         <SITAMap
