d228296c4499a91934c57c434f71da20cffa6da7
SLASHBUTCODING <22-01684@g.batstate-u.edu.ph>
2026-04-11 12:08:13 +0800

    Fix driver home map - add min-h-0 for proper flex rendering

--- src/app/pages/driver/Home.tsx
+++ src/app/pages/driver/Home.tsx
@@ -251,7 +251,7 @@ export function DriverHome() {
       </div>
 
       {/* Map */}
-      <div className="relative z-10 flex-1 mx-4 rounded-2xl overflow-hidden">
+      <div className="relative z-10 flex-1 mx-4 rounded-2xl overflow-hidden min-h-0">
         <SITAMap
           driverLocation={currentCoords ? [currentCoords.lat, currentCoords.lng] : undefined}
           className="w-full h-full"
